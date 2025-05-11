c9a71ff7ea454d1b543be583438dbb590d651dfa
dion lou <dion@nowthatsamazing.co.uk>
2025-05-11 20:13:13 +0100

    Continued work on map styling - added tooltip and outline for selected buildings; need to make tooltip static per building (?) and add a slight outline for hover

--- src/components/CampusMap.jsx
+++ src/components/CampusMap.jsx
@@ -1,36 +1,70 @@
-import React, { useState, useMemo } from 'react'
+import React, { useState } from 'react'
 import DeckGL from '@deck.gl/react'
 import { Map } from 'react-map-gl/mapbox'
-import { GeoJsonLayer, TextLayer } from 'deck.gl'
+import { GeoJsonLayer} from 'deck.gl'
 import 'mapbox-gl/dist/mapbox-gl.css'
 import campusData from '../tempData/campusMapGeo.json'
 
+
 const CampusMap = () => {
-    const [viewport, setViewport] = useState({
+  const [selectedBuilding, setSelectedBuilding] = useState(null)
+  const [hoveredInfo, setHoveredInfo] = useState(null)
+  
+  const [viewport, setViewport] = useState({
         latitude: 50.742719,
         longitude: -1.895220,
         zoom: 18,
         bearing: 17
     })
 
-    const layers = useMemo(() => [
+    const layers = [
         new GeoJsonLayer({
-          id: 'temp-building',
+          id: 'buildings-fill',
           data: campusData,
           filled: true,
           wireframe: false,
           getFillColor: d => d.properties.color,
           getLineColor: [0, 0, 0, 0],
           pickable: true,
-          material: {
-            ambient: 0.4, 
-            diffuse: 0.6
-          },
-          transitions: {
-            getFillColor: 1000
-          }
-        })
-      ], []);
+          onHover: info => setHoveredInfo(info),
+          onClick: info => setSelectedBuilding(info.object),
+        }),
+        new GeoJsonLayer({
+          id: 'selected-building-outline',
+          data: selectedBuilding ? {type: 'FeatureCollection', features: [selectedBuilding]} : null,
+          filled: false,
+          stroked: true,
+          getLineColor: [153, 0, 0],
+          lineWidthMinPixels: 1,
+          pickable: false
+        }),
+      ]
+      
+    const renderTooltip = () => {
+      const info = hoveredInfo?.object?.properties || selectedBuilding
+      if (!info || !hoveredInfo) return null
+      
+      return (
+        <div className='absolute z-10 max-w-xs bg-white border border-gray-200 rounded-xl shadow-lg px-2 text-sm py-2' 
+        style={{ 
+          left: hoveredInfo.x + 10, 
+          top: hoveredInfo.y + 10,
+          pointerEvents: 'none',
+          minWidth: '140px',
+          }}
+          >
+
+          <div className='relative'> 
+            <div className='text-gray-800 font-light'>{info.name}</div>
+            <div className='text-gray-600 font-light'>placeholder</div>
+          </div>
+
+          <div className='absolute w-3 h-3 left-1/2 bg-white border-b border-r border-gray-200 transform rotate-45 top-full -mt-1.5
+          '/>
+      
+        </div>
+      )
+    }
   
     return (
         <div className='w-full h-screen ' style={{ position: 'relative'}}>
@@ -42,10 +76,10 @@ const CampusMap = () => {
         <Map 
             mapboxAccessToken={import.meta.env.VITE_MAPBOX_ACCESS_TOKEN}
             style={{ width: '100%', height: '100%'}}
-            mapStyle={"mapbox://styles/mapbox/light-v11"}
-            >
-        </Map>
+            mapStyle={"mapbox://styles/dionlou/cmae457b600qq01qyalrd3uof"}
+        />
         </DeckGL>
+        {renderTooltip()}
         </div>
   )
 }
