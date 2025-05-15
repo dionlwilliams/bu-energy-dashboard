ee4482d38feba16c83012c7f3dd89c1e92d8403a
dionlou <dionlwilliams@outlook.com>
2025-05-15 22:31:58 +0100

    colour scale added to buildings - working on getting tooltips in center of buildings

--- src/components/CampusMap.jsx
+++ src/components/CampusMap.jsx
@@ -1,13 +1,22 @@
 import React, { use, useMemo, useState } from 'react'
 import DeckGL from '@deck.gl/react'
 import { Map } from 'react-map-gl/mapbox'
-import { GeoJsonLayer} from 'deck.gl'
+import { GeoJsonLayer, WebMercatorViewport} from 'deck.gl'
+import { centroid } from '@turf/turf'
 import 'mapbox-gl/dist/mapbox-gl.css'
 import campusData from '../tempData/campusMapGeo.json'
 
 const CampusMap = ({buildingData}) => {
   const [selectedBuilding, setSelectedBuilding] = useState(null)
   const [hoveredInfo, setHoveredInfo] = useState(null)
+
+  const COLOUR_SCALE = [
+    [224, 225, 251, 255],
+    [176, 178, 247, 255],
+    [134, 137, 244, 255], 
+    [99, 102, 241, 255]   
+  ]
+
   
   const [viewport, setViewport] = useState({
         latitude: 50.742719,
@@ -31,17 +40,29 @@ const CampusMap = ({buildingData}) => {
     const processedCampusData = useMemo(() => {
       if (!campusData?.features) return null
 
+      const energyValues = Object.values(energyLookup)
+      const minEnergy = Math.min(...energyValues)
+      const maxEnergy = Math.max(...energyValues)
+      const energyRange = maxEnergy - minEnergy
+
       return {
         ...campusData,
         features: campusData.features.map(feature => {
           const buildingName = feature.properties.name
           const energyUsage = energyLookup[buildingName] || 0
           
+          let colourIndex = 0
+          if (energyRange > 0) {
+            const ratio = (energyUsage - minEnergy) / energyRange
+            colourIndex = Math.floor(ratio * 3)
+            colourIndex = Math.min(3, Math.max(0, colourIndex))
+          }
           return {
             ...feature,
             properties: {
               ...feature.properties,
               kWh: energyUsage,
+              color: COLOUR_SCALE[colourIndex]
             },
           }
         }),
@@ -58,7 +79,27 @@ const CampusMap = ({buildingData}) => {
           getLineColor: [0, 0, 0, 0],
           getFillColor: d => d.properties.color,
           pickable: true,
-          onHover: info => setHoveredInfo(info),
+          onHover: info => {
+            if (info.object) {
+              const center = centroid(info.object)
+              const coords = center.geometry.coordinates
+              
+              const viewport = new WebMercatorViewport({
+                width: window.innerWidth,
+                height: window.innerHeight,
+                ...info.viewState
+              })
+
+              const [x, y] = viewport.project(coords)
+
+              setHoveredInfo({
+                object: info.object, 
+                x, 
+                y })
+            } else {
+              setHoveredInfo(null)
+            }
+          },
           onClick: info => setSelectedBuilding(info.object),
         }),
         new GeoJsonLayer({
@@ -66,21 +107,30 @@ const CampusMap = ({buildingData}) => {
           data: selectedBuilding ? {type: 'FeatureCollection', features: [selectedBuilding]} : null,
           filled: false,
           stroked: true,
-          getLineColor: [153, 0, 0],
+          getLineColor: d => {
+            const [r, g, b] = d.properties.color
+            return [r - 30, g - 30, b - 30]
+          },
           lineWidthMinPixels: 1,
           pickable: false
         }),
       ]
       
     const renderTooltip = () => {
-      const info = hoveredInfo?.object?.properties || selectedBuilding
+      console.log('hoveredInfo', hoveredInfo)
+      const info = hoveredInfo?.object?.properties || selectedBuilding?.properties
+      if (!info || !hoveredInfo) {
+        console.log("wttf")
+      }
       if (!info || !hoveredInfo) return null
+      
       
       return (
         <div className='absolute z-10 max-w-xs bg-white border border-gray-200 rounded-xl shadow-lg px-2 text-sm py-2' 
         style={{ 
-          left: hoveredInfo.x + 10, 
-          top: hoveredInfo.y + 10,
+          left: hoveredInfo.x, 
+          top: hoveredInfo.y,
+          transform: 'translate(-50%, -100%)',
           pointerEvents: 'none',
           minWidth: '140px',
           }}
