e5781f2c3b8fb087fa8c0637f6973b5e51468fc6
dionlou <dionlwilliams@outlook.com>
2025-05-21 21:32:31 +0100

    More work on building detail div styling - sizing needs to be done

--- src/components/CampusMap.jsx
+++ src/components/CampusMap.jsx
@@ -10,6 +10,7 @@ import LineGraph from '../components/overview/LineGraph'
 import PieGraph from '../components/overview/PieGraph'
 import { normaliseDataset } from '../utils/energyConversion'
 import BuildingTooltip from './BuildingTooltip'
+import { Zap, PieChart, Cloud, Home } from 'lucide-react'
 
 const CampusMap = ({buildingData}) => {
   const [selectedBuilding, setSelectedBuilding] = useState(null)
@@ -243,25 +244,85 @@ useEffect(() => {
         <SlidePanel
         isOpen={!!selectedBuilding}
         onClose={() => setSelectedBuilding(null)}
+        headerContent={
+          normalisedDetails && (
+            <div className='flex items-start justify-between gap-4 flex-1'>
+              <div className='flex flex-col gap-1'>
+                <div className='flex items-center gap-3'>
+                  <span className='
+                    flex items-center justify-center 
+                    w-10 h-10 rounded-full 
+                  bg-indigo-300 text-indigo-800
+                    text-base font-semibold
+                    '>
+                      A+
+                  </span>  
+                  <h2 className='text-xl font-light tracking-wide text-stone-700'>
+                    {selectedBuilding?.properties?.name}
+                  </h2>
+              </div>
+              <p className='text-sm text-gray-500 tracking-wider'>
+                Based on annual energy usage (kWh/m²)
+              </p>
+            </div>
+          </div>
+          ) 
+        }
         >
-          {normalisedDetails && (
-            <div className='space-y-6'>
-              <div className='flex items-center gap-2'>
-                <span className='inline-block px-3 py-1 text-xs font-normal bg-green-100 text-green-700 rounded-full'>
-                  Efficient
-                </span>
-                <span className='text-sm text-gray-500'>
-                  Based on kWh/m²
-                </span>
+        
+        {normalisedDetails && (
+          <div className='flex flex-col gap-6 text-gray-800 text-sm'>
+            
+            <div className='bg-gray-50 rounded-lg p-2 shadow-sm flex flex-col items-center'>
+              <div className='flex items-center gap-2 mb-2 align-middle'>
+                <Zap size={25} className='text-yellow-400' />
+                <h3 className="text-md font-light text-xl tracking-wide mb-2">Yearly Energy Usage</h3>
+              </div>
+              
+              <LineGraph data={normalisedDetails.monthlyUsage} />
+
+              <div className='w-full mt-2 flex justify-center'>
+                <div className='relative h-5 w-6/8 bg-gray-200 rounded-full overflow-hidden mx-auto'>
+                  <div 
+                  className='absolute h-full bg-yellow-400 rounded-full transition-all duration-300' 
+                  style={{ width: '7%'}}
+                  />
+                </div>
+              </div>
+    
+              <p className='mt-2 text-gray-600 text-lg'>
+                <span className='font-medium text-gray-800 '>X%</span> less than campus average
+              </p>
+            </div>
+
+           
+
+            <div className='bg-gray-50 rounded-lg p-2 shadow-sm flex flex-col items-center'>
+              <div className='flex items-center gap-2 mb-2'>
+                <PieChart size={25} className='text-indigo-500' />
+                <h3 className='text-md font-light text-xl tracking-wide mb-2'>Energy Breakdown</h3>
               </div>
 
-              <LineGraph data={normalisedDetails.monthlyUsage} />
               <PieGraph data={normalisedDetails.energySources} />
-            <div/>
             </div>
-          )}
+
+              <div className='grid grid-cols-2 gap-4 w-full'>
+                <div className='bg-gray-50 rounded-lg p-3 flex flex-col items-center shadow-sm'>
+                  <Cloud className='text-gray-400 mb-1' size={22} />
+                  <p className='font-bold text-base text-gray-700'>X kg</p>
+                  <p className='text-gray-500 text-[11px]'>CO₂ / year</p>
+                </div>
+                <div className='bg-gray-50 rounded-lg p-3 flex flex-col items-center shadow-sm'>
+                  <Home className='text-gray-400 mb-1' size={22} />
+                  <p className='font-bold text-base text-gray-700'>27</p>
+                  <p className='text-gray-500 text-[11px]'>UK homes / year</p>
+                </div>
+              </div>
+            </div>
+          
+        )}
         </SlidePanel>
-        </div>
+      </div>
   )
 }
 
--- src/components/SlidePanel.jsx
+++ src/components/SlidePanel.jsx
@@ -1,32 +1,34 @@
 import { X } from 'lucide-react'
 import { motion, AnimatePresence } from 'framer-motion'
 
-export const SlidePanel = ({isOpen, onClose, children }) => {
+export const SlidePanel = ({ isOpen, onClose, headerContent, children }) => {
     return (
         <AnimatePresence>
             {isOpen && (
                 <motion.div
-            initial={{ x: '-100%' }}
-            animate={{ x: 0 }}
-            exit={{ x: '-100%' }}
-            transition={{ type: 'tween', duration: 0.4  }}
-            className='fixed top-0 left-0 h-full w-100 bg-neutral-50 shadow-xl z-[1001] pointer-events-auto'
-            >
-                <div className='h-full flex flex-col'>
-                    <div className='flex justify-between items-center p-6 border-b'>
-                        <button
-                            onClick={onClose}
-                            className='p-1 hover:bg-gray-100 rounded-full'
-                        >
-                            <X className='w-6 h-6 text-gray-600' />
-                        </button>
-                    </div>
+                initial={{ x: '-100%' }}
+                animate={{ x: 0 }}
+                exit={{ x: '-100%' }}
+                transition={{ type: 'tween', duration: 0.4  }}
+                className='fixed top-0 left-0 h-full w-100 bg-neutral-50 shadow-xl z-[1001] pointer-events-auto'
+                >
+                    <div className='h-full flex flex-col'>
+                        <div className='flex justify-between items-center p-6'>
+                            {headerContent}
+                            <button
+                                onClick={onClose}
+                                className='p-1 hover:bg-gray-100 rounded-full'
+                            >
+                                <X className='w-6 h-6 text-gray-600' />
+                            </button>
+                        </div>
+                        <hr className="border-gray-300"/>
 
-                    <div className='flex-1 overflow-auto p-6'>
-                        {children}
+                        <div className='flex-1 overflow-auto p-6'>
+                            {children}
+                        </div>
                     </div>
-                </div>
-            </motion.div>
+                </motion.div>
             )}
         </AnimatePresence>
     )
